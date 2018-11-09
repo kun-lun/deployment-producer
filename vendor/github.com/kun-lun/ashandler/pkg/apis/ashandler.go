95d049ddf2edb86c57177019c3b46e2095d38888
Andy Liu <andyliuliming@outlook.com>
2018-11-09 22:29:03 +0800

    fix

--- vendor/github.com/kun-lun/ashandler/pkg/apis/ashandler.go
+++ vendor/github.com/kun-lun/ashandler/pkg/apis/ashandler.go
@@ -3,9 +3,15 @@ package apis
 import (
 	"github.com/kun-lun/artifacts/pkg/apis/deployments"
 	"github.com/kun-lun/ashandler/generator"
-	"github.com/kun-lun/common/logger"
 	"github.com/kun-lun/common/storage"
 )
+
+type logger interface {
+	Step(string, ...interface{})
+	Printf(string, ...interface{})
+	Println(string)
+	Prompt(string) bool
+}
 
 type ASHandler struct {
 	asGenerator generator.ASGenerator
@@ -13,7 +19,7 @@ type ASHandler struct {
 
 func NewASHandler(
 	stateStore storage.Store,
-	logger *logger.Logger,
+	logger logger,
 ) ASHandler {
 	return ASHandler{
 		asGenerator: generator.NewASGenerator(stateStore, logger),
